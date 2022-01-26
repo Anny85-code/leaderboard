564af077c6ce5c99b1d632829b55fc04819412bc
uae <aniekaudo@yahoo.com>
2022-01-26 19:35:04 +0100

    Added api and render.js file

---
+++ src/api.js
@@ -0,0 +1,30 @@
+const apiKey = 'bdn30omCICeExhdmH62P';
+
+const setData = async (score) => {
+  const response = await fetch(
+    `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${apiKey}/scores`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(score),
+    }
+  );
+
+  const { result } = await response.json();
+
+  return result;
+};
+
+const getData = async () => {
+  const response = await fetch(
+    `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${apiKey}/scores`
+  );
+
+  const { result } = await response.json();
+
+  return result;
+};
+
+export { getData, setData };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import './style.css';
+
+import { getData, setData } from './api.js';
+import renderList from './render.js';
+
+const refreshBtn = document.querySelector('.refresh');
+const form = document.querySelector('form');
+
+refreshBtn.addEventListener('click', async () => {
+  const list = await getData();
+
+  renderList(list);
+});
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const score = {
+    user: form.elements.username.value.trim(),
+    score: form.elements.score.value.trim(),
+  };
+
+  form.reset();
+
+  await setData(score);
+});
